aeb031ea5b235d194afaf35611dafdfbb42f1f4b
DonquixoteSabo <mixboksu@gmail.com>
2023-03-25 12:45:58 +0100

    Add posts api router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
-import {SignInButton, SignOutButton, useUser} from "@clerk/nextjs";
-import { type NextPage } from "next";
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import type { NextPage } from "next";
 import Head from "next/head";
 
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   const user = useUser();
 
+  const { data } = api.posts.getAll.useQuery();
+
+  console.log(data);
   return (
     <>
       <Head>
@@ -17,13 +18,12 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-          <div>
-              {!user.isSignedIn ? (
-                  <SignInButton />
-              ) : (
-                  <SignOutButton />
-              )}
-          </div>
+        <div>{!user.isSignedIn ? <SignInButton /> : <SignOutButton />}</div>
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
